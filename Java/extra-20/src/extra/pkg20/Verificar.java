f92c32df5e851b8d097c3d311093215ca08d9df5
roberto <robertogarciahuidobro@gmail.com>
2025-01-14 11:36:02 +0100

    resolver sudoku 12/01/25

---
+++ Java/extra-20/src/extra/pkg20/Verificar.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package extra.pkg20;
+
+/**
+ *
+ * @author rober
+ */
+public class Verificar {
+    
+}
