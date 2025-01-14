d0f370dc7e1d73a772b018c690b1719627187aa1
roberto <robertogarciahuidobro@gmail.com>
2025-01-14 11:51:51 +0100

    extra-20 verificar que un sudoku está bien resuelto

--- Java/extra-20/src/extra/pkg20/Extra20.java
+++ Java/extra-20/src/extra/pkg20/Extra20.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package extra.pkg20;
+
+import extra.pkg20.utils.Verificar;
 
 /**
  *
@@ -10,11 +8,28 @@ package extra.pkg20;
  */
 public class Extra20 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int[][] sudoku = {
+        {4, 1, 3, 8, 2, 5, 6, 7, 9},
+        {5, 6, 7, 1, 4, 9, 8, 3, 2},
+        {2, 8, 9, 7, 3, 6, 1, 4, 5},
+        {1, 9, 5, 4, 6, 2, 7, 8, 3},
+        {7, 2, 6, 9, 8, 3, 5, 1, 4},
+        {3, 4, 8, 5, 1, 7, 2, 9, 6},
+        {8, 5, 1, 6, 9, 4, 3, 2, 7},
+        {9, 7, 2, 3, 5, 8, 4, 6, 1},
+        {6, 3, 4, 2, 7, 1, 9, 5, 8}
+    };
+    Verificar verificar = new Verificar();
+    
+        if (verificar.estaCorrecto(sudoku)) {
+            System.out.println("está correcto");
+        } else {
+            System.out.println("NO está correcto");
+        }
+    
+    
+
     }
     
 }
---
+++ Java/extra-20/src/extra/pkg20/utils/Verificar.java
@@ -0,0 +1,65 @@
+package extra.pkg20.utils;
+
+/**
+ *
+ * @author rober
+ */
+public class Verificar {
+    public boolean estaCorrecto(int[][] sudoku){
+        
+        // lee el array sudoku
+        for (int i = 0; i < sudoku.length; i++) {
+            for (int j = 0; j < sudoku[i].length; j++) {
+                for (int k = 0; k <= 9; k++) {
+                    // verifica que no existan numeros repetidos en fila, columna y cuadro
+                    if(hayEnColumna(sudoku, i, k) && hayEnFila(sudoku, j, k) && hayEnCuadro(sudoku, i, j, k)){
+                        return false; // devuelve false si está mal hecho
+                    }
+                }
+            }
+            
+        }
+        
+        return true; // devuelve true si está bien hecho
+        
+    }
+    
+    public boolean hayEnFila(int[][] sudoku, int fila, int numero){
+        // si hay numeros repetidos en la fila será true 
+        for (int i = 0; i < sudoku.length; i++) {
+            if (sudoku[fila][i] == numero) {
+                return true; // si el numero se repite devuelve true
+            }
+        }
+        
+        return false; // si el numero no se repite devuelve false
+    }
+    
+    public boolean hayEnColumna(int[][] sudoku, int columna, int numero){
+        // si hay numeros repetido en la columna devuelve true
+        for (int i = 0; i < sudoku.length; i++) {
+            if (sudoku[i][columna]== numero) {
+                return true; // si el numero está en la fila devuelve true
+            }
+        }
+        
+        return false; // si el numero no está en la columna devuelve false
+    }
+    
+    public boolean hayEnCuadro(int[][] sudoku, int fila, int columna, int numero){
+        // inicia el lugar en el que se van a ver si los numeros se repiten en el cuadro
+        int inicioFila = (fila / 3)*3;
+        int inicioColumna = (columna /3)*3;
+        
+        for (int i = inicioFila; i < inicioFila + 3; i++) {
+            for (int j = inicioColumna; j < inicioColumna; j++) {
+                // verifica que no exista el numero en el cuadrante
+                if(sudoku[i][j]==numero){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
