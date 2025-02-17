0a81dd60a14641a45c7fa1b4d71a073262954624
roberto <robertogarciahuidobro@gmail.com>
2025-02-17 18:38:31 +0100

    retos de programacion 100 extra-29 17-02-25

---
+++ Java/extra-29/src/extra/pkg29/Extra29.java
@@ -0,0 +1,59 @@
+package extra.pkg29;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ *
+ * @author rober
+ * retos de programación 100: la palabra de 100 puntos
+ * sumar los valores de cada letra. la mejor palabra es la que la suma de los valores de las letras es 100
+ */
+public class Extra29 {
+    
+    // traducotor de letras a números
+    public static int traductorletranumero(char letra){
+        // crea u HashMap con todas las letras del alfabeto español
+        Map<Character, Integer> valorLetras = new HashMap<Character, Integer>();
+        String letras = "abcdefghijklmnñopqrstuvwxyz";
+        for (int i = 0; i < letras.length(); i++) {
+            valorLetras.put(letras.charAt(i), i+1);
+        }
+        
+        // devuelve el valor de la letra según sus valores
+        return valorLetras.get(letra);
+        
+        
+    }
+    // suma de los valores de las letras
+    public static int palabraNumero(String palabra){
+        int resultado = 0;
+        
+        // divide el String en letras y llama al traductor
+        char[] letras = palabra.toCharArray();
+        for(char letra : letras){
+            resultado+=traductorletranumero(letra);
+        }
+        
+        return resultado;
+    }
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String input=scn.nextLine();
+        
+        // muestra los resultado de los valores de las palabras
+        if(palabraNumero(input) > 100){
+            System.out.println("perdisre");
+            System.out.println(palabraNumero(input));
+        } else if(palabraNumero(input)==100){
+            System.out.println("ganaste");
+            System.out.println(palabraNumero(input));
+        } else {
+            System.out.println("quedaste por debajo");
+            System.out.println(palabraNumero(input));
+        }
+        
+    }
+    
+}
